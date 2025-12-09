a08ed21e64443b6d56356d410e0ab682ed5b1b82
mymx9 <parker.cai@outlook.com>
2025-12-09 11:17:48 +0800

    add timesfm support

--- download.sh
+++ download.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-pip install -U huggingface_hub
+# pip install -U huggingface_hub
 
 export HF_ENDPOINT=https://hf-mirror.com
 
 hf download Salesforce/moirai-1.0-R-small --local-dir ./pretrained/Salesforce/moirai-1.0-R-small
 hf download Salesforce/moirai-1.1-R-small --local-dir ./pretrained/Salesforce/moirai-1.1-R-small
 hf download Salesforce/moirai-2.0-R-small --local-dir ./pretrained/Salesforce/moirai-2.0-R-small
+hf download amazon/chronos-2 --local-dir ./pretrained/amazon/chronos-2
+hf download google/timesfm-2.5-200m-pytorch --local-dir ./pretrained/google/timesfm-2.5-200m-pytorch
